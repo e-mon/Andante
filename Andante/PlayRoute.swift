cd971659a719a32da6eb53ca1f4cac9403bb231d
e-mon <emon18@icloud.com>
2014-10-28 19:00:12 +0900

    Implement PlayRouteManager Class and some bug fix.

--- Andante/PlayRoute.swift
+++ Andante/PlayRoute.swift
@@ -8,13 +8,17 @@
 
 import Foundation
 import CoreData
+import CoreLocation
 
+//CoreDataがobjcライブラリらしく、これがないとクラスを認識してくれない
+@objc(PlayRoute)
 class PlayRoute: NSManagedObject {
 
     @NSManaged var userName: String
-    @NSManaged var region: AnyObject
+    @NSManaged var region: CLRegion
     @NSManaged var songName: String
-    @NSManaged var artistName: AnyObject
+    @NSManaged var artistName: String
+    //FIXME : インスタンスをいちいち生成してオブジェクトを保存するまでもないので、longで保存でいいかも。
     @NSManaged var timestamp: NSDate
 
 }
