f99f6874de87004ab0a9a624d5803ab6229f7238
yubessy <yubessy0@gmail.com>
2014-11-28 00:04:25 +0900

    Revise BackgroundPlayController entirely

--- Andante/BackgroundPlayController.swift
+++ Andante/BackgroundPlayController.swift
@@ -17,9 +17,7 @@ class BackgroundPlayController: NSObject, CLLocationManagerDelegate {
     private let playRouteManager = PlayRouteManager()
 
     private var lastHitLocation: CLLocation!
-    private var lastPlayedMediaItem: MPMediaItem!
-
-    private let minDistance = 10.0
+    private var lastPlayedItem: MPMediaItem!
 
     override init() {
         super.init()
@@ -28,61 +26,50 @@ class BackgroundPlayController: NSObject, CLLocationManagerDelegate {
     }
 
     internal func startUpdatingLocation() {
-        println("**startUpdatingLocation**")
+        let status = CLLocationManager.authorizationStatus()
+        if status == CLAuthorizationStatus.NotDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+
         self.lastHitLocation = nil
-        self.lastPlayedMediaItem = nil
+        self.lastPlayedItem = nil
         self.locationManager.startUpdatingLocation()
     }
 
     internal func stopUpdatingLocation() {
-        println("**stopUpdatingLocation**")
+        self.systemMusicPlayer.stop()
         self.locationManager.stopUpdatingLocation()
     }
 
     /* CLLocationManagerDelegate methods */
 
     internal func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        println("**didUpdateLocations**")
-        let newestLocation: CLLocation = locations.last as CLLocation
-        println(newestLocation)
-
-        if self.lastHitLocation != nil && self.lastHitLocation.distanceFromLocation(newestLocation) < self.minDistance {
-            println("--too near from last location--")
-            return
-        }
-
         // TODO: 以下の場合の挙動については詳しく考える必要あり
         // * 既に曲が再生中
         //     * その曲がAndanteによって再生された
         //     * その曲が他のアプリによって再生された
         // とりあえず現在はsystemMusicPlayerがStoppedまたはPausedの場合のみ再生を開始する
-
         let state: MPMusicPlaybackState = self.systemMusicPlayer.playbackState
         if state != MPMusicPlaybackState.Stopped && state != MPMusicPlaybackState.Paused {
-            println("--system music player is occupied--")
             return
         }
 
-        let item: MPMediaItem! = self.playRouteManager.getMediaPlayItem(newestLocation.coordinate, side: 40.0)
+        let newestLocation: CLLocation = locations.last as CLLocation
+        let newItem: MPMediaItem! = self.playRouteManager.getMediaPlayItem(newestLocation.coordinate, side: 40.0)
 
-        if item == nil {
-            println("--item not found--")
+        if newItem == nil {
             return
         }
 
-        if self.lastPlayedMediaItem != nil && item == self.lastPlayedMediaItem {
-            println("--the same as last item--")
+        if self.lastPlayedItem != nil && newItem == self.lastPlayedItem {
             return
         }
 
-        println("--new item found--")
-        println(item)
-
-        let collection = MPMediaItemCollection(items: [item])
+        let collection = MPMediaItemCollection(items: [newItem])
         self.systemMusicPlayer.setQueueWithItemCollection(collection)
         self.systemMusicPlayer.play()
 
         self.lastHitLocation = newestLocation
-        self.lastPlayedMediaItem = item
+        self.lastPlayedItem = newItem
     }
 }
