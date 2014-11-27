eda31de67e43fd6fdc703a64ed24fefbc7260b7d
yubessy <yubessy0@gmail.com>
2014-11-27 19:15:04 +0900

    Revise BackgroundPlayContoller class

--- Andante/BackgroundPlayController.swift
+++ Andante/BackgroundPlayController.swift
@@ -17,9 +17,7 @@ class BackgroundPlayController: NSObject, CLLocationManagerDelegate {
     private let playRouteManager = PlayRouteManager()
 
     private var lastHitLocation: CLLocation!
-    private var lastPlayedMediaItem: MPMediaItem!
-
-    private let minDistance = 10.0
+    private var lastPlayedItem: MPMediaItem!
 
     override init() {
         super.init()
@@ -28,8 +26,13 @@ class BackgroundPlayController: NSObject, CLLocationManagerDelegate {
     }
 
     internal func startUpdatingLocation() {
+        let status = CLLocationManager.authorizationStatus()
+        if status == CLAuthorizationStatus.NotDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+
         self.lastHitLocation = nil
-        self.lastPlayedMediaItem = nil
+        self.lastPlayedItem = nil
         self.locationManager.startUpdatingLocation()
     }
 
@@ -40,38 +43,32 @@ class BackgroundPlayController: NSObject, CLLocationManagerDelegate {
     /* CLLocationManagerDelegate methods */
 
     internal func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        let newestLocation: CLLocation = locations.last as CLLocation
-
-        if self.lastHitLocation != nil && self.lastHitLocation.distanceFromLocation(newestLocation) < self.minDistance {
-            return
-        }
-
         // TODO: 以下の場合の挙動については詳しく考える必要あり
         // * 既に曲が再生中
         //     * その曲がAndanteによって再生された
         //     * その曲が他のアプリによって再生された
         // とりあえず現在はsystemMusicPlayerがStoppedまたはPausedの場合のみ再生を開始する
-
         let state: MPMusicPlaybackState = self.systemMusicPlayer.playbackState
         if state != MPMusicPlaybackState.Stopped && state != MPMusicPlaybackState.Paused {
             return
         }
 
-        let item: MPMediaItem! = self.playRouteManager.getMediaPlayItem(newestLocation.coordinate, side: 40.0)
+        let newestLocation: CLLocation = locations.last as CLLocation
+        let newItem: MPMediaItem! = self.playRouteManager.getMediaPlayItem(newestLocation.coordinate, side: 40.0)
 
-        if item == nil {
+        if newItem == nil {
             return
         }
 
-        if self.lastPlayedMediaItem != nil && item == self.lastPlayedMediaItem {
+        if self.lastPlayedItem != nil && newItem == self.lastPlayedItem {
             return
         }
 
-        let collection = MPMediaItemCollection(items: [item])
+        let collection = MPMediaItemCollection(items: [newItem])
         self.systemMusicPlayer.setQueueWithItemCollection(collection)
         self.systemMusicPlayer.play()
 
         self.lastHitLocation = newestLocation
-        self.lastPlayedMediaItem = item
+        self.lastPlayedItem = newItem
     }
 }
